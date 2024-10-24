1631e125324904897edad213b99d84044be79ee5
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-10-24 07:08:30 -0500

    Added DAO for Reservation, Room, and User.

--- backend/src/main/java/com/vpalz/hotellosterrenos/dao/ReservationDAO.java
+++ backend/src/main/java/com/vpalz/hotellosterrenos/dao/ReservationDAO.java
@@ -1,4 +1,24 @@
 package com.vpalz.hotellosterrenos.dao;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReservationDAO {
+    private Long id;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    private int numberOfChildren;
+    private int numberOfAdults;
+    private int totalNumberOfGuests;
+
+    private String reservationConfirmationCode;
+
+    private UserDAO user;
+    private RoomDAO room;
 }
