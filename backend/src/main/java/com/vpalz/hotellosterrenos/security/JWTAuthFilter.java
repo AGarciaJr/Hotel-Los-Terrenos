de6f6dbb88e6c8269ff947799e340b24b0976027
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-10-24 08:26:00 -0500

    Added JWTAuthFilter. This file needs some work because I think it causes errors in Login.

--- backend/src/main/java/com/vpalz/hotellosterrenos/security/JWTAuthFilter.java
+++ backend/src/main/java/com/vpalz/hotellosterrenos/security/JWTAuthFilter.java
@@ -1,4 +1,58 @@
 package com.vpalz.hotellosterrenos.security;
 
-public class JWTAuthFilter {
+import com.vpalz.hotellosterrenos.service.CustomUserDetailsService;
+import com.vpalz.hotellosterrenos.utils.JWTUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private CustomUserDetailsService myUserDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+
+        if(authorizationHeader == null || !authorizationHeader.isBlank()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authorizationHeader.substring(7);
+        userEmail = jwtUtils.extractUsername(jwtToken);
+
+        if(userEmail == null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = myUserDetailService.loadUserByUsername(userEmail);
+            if(jwtUtils.validateToken(jwtToken, userDetails)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                securityContext.setAuthentication(token);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
