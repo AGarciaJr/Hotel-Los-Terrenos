441328080d388259e4cc23b78350169a42b7b29b
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-10-24 08:22:21 -0500

    Added Service Implementations for Entities.

--- backend/src/main/java/com/vpalz/hotellosterrenos/service/implementations/ReservationService.java
+++ backend/src/main/java/com/vpalz/hotellosterrenos/service/implementations/ReservationService.java
@@ -1,4 +1,168 @@
 package com.vpalz.hotellosterrenos.service.implementations;
 
-public class ReservationService {
+import com.vpalz.hotellosterrenos.dao.ReservationDAO;
+import com.vpalz.hotellosterrenos.dao.Response;
+import com.vpalz.hotellosterrenos.entity.Reservation;
+import com.vpalz.hotellosterrenos.entity.Room;
+import com.vpalz.hotellosterrenos.entity.User;
+import com.vpalz.hotellosterrenos.exception.MyException;
+import com.vpalz.hotellosterrenos.repo.ReservationRepository;
+import com.vpalz.hotellosterrenos.repo.RoomRepository;
+import com.vpalz.hotellosterrenos.repo.UserRepository;
+import com.vpalz.hotellosterrenos.service.interfaces.IReservationService;
+import com.vpalz.hotellosterrenos.service.interfaces.IRoomService;
+import com.vpalz.hotellosterrenos.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReservationService implements IReservationService {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    /*
+    @Autowired
+    private IRoomService roomService;
+     */
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Response saveReservation(Long roomId, Long userId, Reservation reservationRequest) {
+        Response response = new Response();
+
+        try {
+            if(reservationRequest.getCheckOutDate().isAfter(reservationRequest.getCheckInDate())) {
+                throw new IllegalArgumentException("Invalid check in/out dates.");
+            }
+
+            Room room = roomRepository.findById(roomId).orElseThrow(() -> new MyException("Room Not Found."));
+            User user = userRepository.findById(userId).orElseThrow(() -> new MyException("User Not Found."));
+
+            List<Reservation> reservations = room.getReservations();
+
+            if(!roomIsAvailable(reservationRequest, reservations)) {
+                throw new MyException("Room not available for selected date range.");
+            }
+
+            reservationRequest.setRoom(room);
+            reservationRequest.setUser(user);
+
+            String reservationConfirmationCode = Utils.generateConfirmationCode(10);
+            reservationRequest.setReservationConfirmationCode(reservationConfirmationCode);
+
+            reservationRepository.save(reservationRequest);
+
+            response.setStatusCode(200);
+            response.setMessage("Successfully Saved Reservation");
+            response.setReservationConfirmationCode(reservationConfirmationCode);
+
+        } catch (MyException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Saving a Reservation " + e.getMessage());
+        }
+
+        return response;
+    }
+
+
+    @Override
+    public Response findReservationByConfirmationCode(String confirmationCode) {
+        Response response = new Response();
+
+        try {
+            Reservation reservation = reservationRepository.findByReservationConfirmationCode(confirmationCode).orElseThrow(() -> new MyException("Reservation Not Found."));
+            ReservationDAO reservationDAO = Utils.mapReservationEntityToReservationDAO(reservation);
+
+            response.setStatusCode(200);
+            response.setMessage("Successfully Saved Reservation");
+            response.setReservation(reservationDAO);
+
+        } catch (MyException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Finding a Reservation " + e.getMessage());
+        }
+
+        return response;
+    }
+
+    @Override
+    public Response getAllReservations() {
+        Response response = new Response();
+
+        try {
+            List<Reservation> reservations = reservationRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+            List<ReservationDAO> reservationDAOs = Utils.mapReservationListEntityToReservationDAOList(reservations);
+
+            response.setStatusCode(200);
+            response.setMessage("Successfully Saved Reservation");
+            response.setReservationList(reservationDAOs);
+
+        } catch (MyException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Getting All Reservations " + e.getMessage());
+        }
+
+        return response;
+    }
+
+    @Override
+    public Response cancelReservation(Long reservationId) {
+        Response response = new Response();
+
+        try{
+            Reservation reservations = reservationRepository.findById(reservationId).orElseThrow(() -> new MyException("Reservation Not Found"));
+            reservationRepository.deleteById(reservationId);
+
+            response.setStatusCode(200);
+            response.setMessage("Successfully Saved Reservation");
+        }catch (MyException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Canceling a Reservation " + e.getMessage());
+        }
+
+        return response;
+    }
+
+    private boolean roomIsAvailable(Reservation reservationRequest, List<Reservation> reservations) {
+        return reservations.stream()
+                .noneMatch(existingReservation ->
+                        reservationRequest.getCheckInDate().equals(existingReservation.getCheckInDate())
+                                || reservationRequest.getCheckOutDate().isBefore(existingReservation.getCheckOutDate())
+                                || (reservationRequest.getCheckInDate().isAfter(existingReservation.getCheckInDate())
+                                && reservationRequest.getCheckInDate().isBefore(existingReservation.getCheckOutDate()))
+                                || (reservationRequest.getCheckInDate().isBefore(existingReservation.getCheckInDate())
+
+                                && reservationRequest.getCheckOutDate().equals(existingReservation.getCheckOutDate()))
+                                || (reservationRequest.getCheckInDate().isBefore(existingReservation.getCheckInDate())
+
+                                && reservationRequest.getCheckOutDate().isAfter(existingReservation.getCheckOutDate()))
+
+                                || (reservationRequest.getCheckInDate().equals(existingReservation.getCheckOutDate())
+                                && reservationRequest.getCheckOutDate().equals(existingReservation.getCheckInDate()))
+
+                                || (reservationRequest.getCheckInDate().equals(existingReservation.getCheckOutDate())
+                                && reservationRequest.getCheckOutDate().equals(reservationRequest.getCheckInDate()))
+                );
+    }
 }
--- backend/src/main/java/com/vpalz/hotellosterrenos/service/implementations/UserService.java
+++ backend/src/main/java/com/vpalz/hotellosterrenos/service/implementations/UserService.java
@@ -1,4 +1,88 @@
 package com.vpalz.hotellosterrenos.service.implementations;
 
-public class UserService {
+import com.vpalz.hotellosterrenos.dao.LoginRequest;
+import com.vpalz.hotellosterrenos.dao.Response;
+import com.vpalz.hotellosterrenos.dao.UserDAO;
+import com.vpalz.hotellosterrenos.entity.User;
+import com.vpalz.hotellosterrenos.exception.MyException;
+import com.vpalz.hotellosterrenos.repo.UserRepository;
+import com.vpalz.hotellosterrenos.service.interfaces.IUserService;
+import com.vpalz.hotellosterrenos.utils.JWTUtils;
+import com.vpalz.hotellosterrenos.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public Response register(User user) {
+        Response response = new Response();
+        try{
+            if(user.getRole() == null || user.getRole().isEmpty()){
+                user.setRole("USER");
+            }
+            if(userRepository.existsByEmail(user.getEmail())){
+                throw new MyException("This email has been registered previously.");
+            }
+
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            User savedUser = userRepository.save(user);
+            UserDAO userDAO = Utils.mapUserEntityToUserDAO(savedUser);
+            response.setStatusCode(200);
+            response.setUser(userDAO);
+            response.setMessage("User registered successfully.");
+        }catch (MyException e){
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Registration" + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public Response login(LoginRequest loginRequest) {
+        return null;
+    }
+
+    @Override
+    public Response getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public Response getUserReservationHistory(String userId) {
+        return null;
+    }
+
+    @Override
+    public Response deleteUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public Response getUserById(String userId) {
+        return null;
+    }
+
+    @Override
+    public Response getUserInfo(String email) {
+        return null;
+    }
 }
