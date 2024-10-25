9a3a42712c6e095f47342c53e79dbf9278ebc1fc
Paul-Becker <paul@beckertech.org>
2024-10-25 17:23:40 -0500

    Update PaymentDAO.java

--- backend/src/main/java/com/vpalz/hotellosterrenos/dao/PaymentDAO.java
+++ backend/src/main/java/com/vpalz/hotellosterrenos/dao/PaymentDAO.java
@@ -4,16 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.List;
 
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PaymentDAO {
-
-    @Data
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public class RoomDAO {
-        private Long id;
-        private BigDecimal balance;
-        private String summary;
-        private ReservationDAO reservation;
-    }
+    private Long id;
+    private BigDecimal balance;
+    private String summary;
+    private ReservationDAO reservation;
 }
