e2933ed9ec98597f0d314d5571b8bd6d15ae6603
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-11-17 20:33:55 -0600

    Updated App to reflect new pages.

--- frontend/hotel-frontend/src/App.js
+++ frontend/hotel-frontend/src/App.js
@@ -10,9 +10,12 @@ import HomePage from './component/home/HomePage';
 import AllRoomsPage from './component/reservation/AllRoomsPage';
 import RoomDetailsPage from './component/reservation/RoomDetailsPage';
 import FindReservationPage from './component/reservation/FindReservationPage';
+import ClerkPage from './component/clerk/ClerkPage';
+import EditRoomPage from './component/clerk/EditRoomPage';
+import AddRoomPage from './component/clerk/AddRoomPage';
 import AdminPage from './component/admin/AdminPage';
-import EditRoomPage from './component/admin/EditRoomPage';
-import AddRoomPage from './component/admin/AddRoomPage';
+import CreateClerkPage from './component/admin/CreateClerkPage';
+import UpdatePasswordPage from './component/admin/UpdatePasswordPage';
 
 function App() {
     return (
@@ -33,11 +36,18 @@ function App() {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/home" element={<HomePage />} />
                         <Route path="/rooms" element={<AllRoomsPage />} />
-                        <Route path="/room-details/:roomId" element={<RoomDetailsPage />} /> {/* Currently Not Working?*/}
+                        <Route path="/room-details/:roomId" element={<RoomDetailsPage />} />
                         <Route path="/find-reservation" element={<FindReservationPage />} />
+
+                        <Route path="/clerk" element={< ClerkPage/>} />
+                        <Route path="/clerk/edit-room" element={<EditRoomPage />} />
+                        <Route path="/clerk/add-room" element={<AddRoomPage />} />
+
                         <Route path="/admin" element={<AdminPage />} />
-                        <Route path="/admin/edit-room" element={<EditRoomPage />} />
-                        <Route path="/admin/add-room" element={<AddRoomPage />} />
+                        <Route path="/admin/create-clerk" element={<CreateClerkPage/>}/>
+                        <Route path="/admin/update-password" element={<UpdatePasswordPage/>}/>
+
+
                         <Route path="/login" element={<LoginPage />} />
                         <Route path="/register" element={<RegisterPage />} />
                         <Route path="*" element={<Navigate to="/" />} /> {/* Redirects invalid paths to HomePage */}
