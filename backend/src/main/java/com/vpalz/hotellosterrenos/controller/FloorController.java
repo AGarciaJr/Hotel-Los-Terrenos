838b49cc7012d958f417e9901263be061650447d
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-12-04 19:43:51 -0600

    Testing Floor Controller.

---
+++ backend/src/main/java/com/vpalz/hotellosterrenos/controller/FloorController.java
@@ -0,0 +1,58 @@
+package com.vpalz.hotellosterrenos.controller;
+
+import com.vpalz.hotellosterrenos.dao.Response;
+import com.vpalz.hotellosterrenos.service.interfaces.IFloorService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@Data
+@RestController
+@RequestMapping("/floors")
+public class FloorController {
+
+    @Autowired
+    private IFloorService floorService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('CLERK')")
+    public ResponseEntity<Response> addNewFloor(
+            @RequestParam(value = "floorName") String floorName,
+            @RequestParam(value = "theme") String theme
+    ) {
+        if (floorName == null || floorName.isBlank() || theme == null || theme.isBlank()) {
+            Response response = new Response();
+            response.setStatusCode(400);
+            response.setMessage("Invalid data. Required fields: Floor Name, Theme");
+            return ResponseEntity.status(response.getStatusCode()).body(response);
+        }
+
+        Response response = floorService.addNewFloor(floorName, theme);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllFloors() {
+        Response response = floorService.getAllFloors();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @GetMapping("/{floorId}")
+    public ResponseEntity<Response> getFloorById(@PathVariable Long floorId) {
+        Response response = floorService.getFloorById(floorId);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PutMapping("/update/{floorId}")
+    @PreAuthorize("hasAuthority('CLERK')")
+    public ResponseEntity<Response> updateFloor(
+            @PathVariable Long floorId,
+            @RequestParam(value = "floorName", required = false) String floorName,
+            @RequestParam(value = "theme", required = false) String theme
+    ) {
+        Response response = floorService.updateFloor(floorId, floorName, theme);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
