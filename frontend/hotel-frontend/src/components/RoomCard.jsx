9817f4cc4a7d25aa9954e0f2563d939aa78feb18
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-12-04 18:31:12 -0600

    Minor changes to RoomCard to better fit the flow.

--- frontend/hotel-frontend/src/components/RoomCard.jsx
+++ frontend/hotel-frontend/src/components/RoomCard.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./RoomCard.css";
 
-const RoomCard = ({ room, onReserveNow, buttonLabel = "Reserve Room" }) => {
+const RoomCard = ({ room, onButtonClick, buttonLabel = "Reserve Room" }) => {
     return (
         <div className="room-card">
-            <img src={room.roomPhotoUrl} alt={room.roomType} />
             <h3>{room.roomType}</h3>
             <p>Price: ${room.roomPrice} / night</p>
             <p>{room.roomDescription}</p>
-            <button onClick={() => onReserveNow(room.id)}>{buttonLabel}</button>
+            <button onClick={() => onButtonClick(room.id)}>{buttonLabel}</button>
         </div>
     );
 };
