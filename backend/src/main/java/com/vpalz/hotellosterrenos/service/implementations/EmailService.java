a6d7f53c575558337997519061add5c54b2923bb
Ax3lJD <axeljd0sniffers@gmail.com>
2024-12-08 17:05:41 -0600

    Email app begin

---
+++ backend/src/main/java/com/vpalz/hotellosterrenos/service/implementations/EmailService.java
@@ -0,0 +1,100 @@
+package com.vpalz.hotellosterrenos.service.implementations;
+
+import com.vpalz.hotellosterrenos.service.interfaces.IEmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class EmailService implements IEmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void sendHtmlEmail(String to, String subject, String templateName, Map<String, Object> variables) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+            Context context = new Context();
+            context.setVariables(variables);
+
+            String htmlContent = templateEngine.process(templateName, context);
+
+            helper.setFrom("hotelvpalz@gmail.com");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            System.err.println("Failed to send HTML email: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void sendSimpleEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("hotelvpalz@gmail.com");
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        } catch (Exception e) {
+            System.err.println("Failed to send email: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void sendWelcomeEmail(String userEmail, String userName) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", userName);
+
+        sendHtmlEmail(
+                userEmail,
+                "Welcome to Hotel Los Terrenos",
+                "emails/welcome-email",
+                variables
+        );
+    }
+
+    @Override
+    public void sendPasswordChangeEmail(String userEmail, String userName) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", userName);
+
+        sendHtmlEmail(
+                userEmail,
+                "Password Updated - Hotel Los Terrenos",
+                "emails/password-change-email",
+                variables
+        );
+    }
+
+    @Override
+    public void sendAccountDeletionEmail(String userEmail, String userName) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", userName);
+
+        sendHtmlEmail(
+                userEmail,
+                "Account Deleted - Hotel Los Terrenos",
+                "emails/account-deletion-email",
+                variables
+        );
+    }
+}
